712425aab3cd6db54e6a84a6b41cdb31081a4a08
gclark0812@gmail.com <gclark0812@gmail.com>
2024-04-01 15:16:27 -0700

    Made script executable

--- scripts/pipe/update_version.sh
+++ scripts/pipe/update_version.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Check Python version and import the appropriate module for loading pyproject.toml
-VERSION_CODE=$(python -c "
+# Determine Python version and use appropriate module for loading pyproject.toml
+VERSION_CODE=$(
+python -c "
 import sys
 if sys.version_info < (3, 11):
-    import toml
-    print(toml.load(open('pyproject.toml', 'r'))['project']['version'])
+    import toml  # for Python versions < 3.11
+    version = toml.load(open('pyproject.toml', 'r'))['project']['version']
 else:
-    import tomllib
-    print(tomllib.loads(open('pyproject.toml', 'rb').read())['project']['version'])
+    import tomllib  # for Python 3.11 and later
+    version = tomllib.loads(open('pyproject.toml', 'rb').read())['project']['version']
+print(version)
 ")
 
-# Create or update .bumpversion.cfg with the current version
+# Create .bumpversion.cfg with the current version
 echo "Creating .bumpversion.cfg with current version $VERSION_CODE"
 echo "[bumpversion]" > .bumpversion.cfg
 echo "current_version = $VERSION_CODE" >> .bumpversion.cfg
@@ -21,6 +23,3 @@ echo "" >> .bumpversion.cfg
 echo "[bumpversion:file:pyproject.toml]" >> .bumpversion.cfg
 echo 'search = version = "{current_version}"' >> .bumpversion.cfg
 echo 'replace = version = "{new_version}"' >> .bumpversion.cfg
-
-# Ensure script is executable
-chmod +x update_version.sh
